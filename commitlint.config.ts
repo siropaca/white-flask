943b64619eac66b6990f5ead66dc56a4b076840a
shirai <ryhshr3@gmail.com>
2025-06-15 11:17:39 +0900

    chore: commitlint設定をTypeScript化

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,6 @@
-export default {
+import type { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
@@ -22,4 +24,6 @@ export default {
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 72]
   }
-}
+};
+
+export default config;
