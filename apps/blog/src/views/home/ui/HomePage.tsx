ca2b5db8ebb5b6c292b56480d93c1e3b4c0f8237
shirai <ryhshr3@gmail.com>
2025-07-06 13:51:12 +0900

    chore(blog): デバッグログ追加

--- apps/blog/src/views/home/ui/HomePage.tsx
+++ apps/blog/src/views/home/ui/HomePage.tsx
@@ -6,7 +6,13 @@ export function HomePage() {
   const { data, error, isLoading } = useHello()
 
   if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Error: {error.message}</div>
+  if (error)
+    return (
+      <div>
+        <p>NEXT_PUBLIC_GRAPHQL_ENDPOINT: {process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT}</p>
+        <div>Error: {error.message}</div>
+      </div>
+    )
 
   return (
     <div>
