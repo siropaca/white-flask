c2025861776329c3230bcf3f661c58ba1b427e4a
shirai <ryhshr3@gmail.com>
2025-07-06 14:58:12 +0900

    feat(blog): GraphQL fetcher でサーバーサイドとクライアントサイドの URL を分離

--- apps/blog/src/entities/hello/index.ts
+++ apps/blog/src/entities/hello/index.ts
@@ -1,1 +1,2 @@
 export { useHello } from './api/use-hello'
+export { getHello } from './api/get-hello'
--- apps/blog/src/views/blog/ui/BlogListPage.tsx
+++ apps/blog/src/views/blog/ui/BlogListPage.tsx
@@ -1,7 +1,14 @@
-export function BlogListPage() {
+import { getHello } from '@/entities/hello'
+
+export async function BlogListPage() {
+  // サーバーサイドで hello クエリをフェッチ
+  const data = await getHello()
+
   return (
     <div>
       <h1>ブログ一覧</h1>
+      <p>サーバーサイドでデータを取得しました</p>
+      <p>GraphQL からの応答: {data?.hello}</p>
       <p>ここにコンテンツ</p>
     </div>
   )
