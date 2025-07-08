700581de69bf085e86e6287eb86cafaa38373f3e
shirai <ryhshr3@gmail.com>
2025-07-08 15:04:45 +0900

    feat(blog): tailwind導入

--- apps/blog/src/views/home/ui/HomePage.tsx
+++ apps/blog/src/views/home/ui/HomePage.tsx
@@ -16,7 +16,8 @@ export function HomePage() {
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
       <p>GraphQL からの応答: {data?.hello}</p>
       <p>ここにコンテンツ</p>
     </div>
